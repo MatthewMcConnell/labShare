c2495002bd97fe2657c1c792c9118c2c3be5987c
Dominic <2253422h@student.gla.ac.uk>
2018-02-26 15:00:46 +0000

    Added stuff to urls.py & models.py

--- appLabShare/models.py
+++ appLabShare/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Student(models.Model):
+    
+
+class UserProfile(models.Model):
+    # links UserProfile to a user model instance
+    user = models.OneToOneField(User)
+
+    # Additional attributes we wish to include
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
