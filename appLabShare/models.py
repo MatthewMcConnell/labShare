0c86b32f6596be0896a4232fd0b98242dbd9e9a7
Matthew McConnell <2235757M@student.gla.ac.uk>
2018-02-26 21:39:11 +0000

    Finished models.py, remember to migrate before using it!

--- appLabShare/models.py
+++ appLabShare/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
-class Student(models.Model):
-    # dummy variable DELETE ON IMPLEMENTATION
-    dummy = models.CharField(max_length = 5)
 
 class UserProfile(models.Model):
     # links UserProfile to a user model instance
@@ -13,43 +9,74 @@ class UserProfile(models.Model):
 
     # Additional attributes we wish to include
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    bio = models.CharField(max_length = 128)
+    degree = models.CharField(max_length = 128)
 
     def __str__(self):
         return self.user.username
 
-# class studentUser (models.Model):
-#     # Will have to inherit from models.User
 
-#     # DummyVariable DELETE ME WHEN IMPLEMENTING
+class Tutor (models.Model):
+    # Is there any other fields????????!!!!!!!!! ##########################################
+    profile = models.OneToOneField (UserProfile)
 
-#     dummy = models.CharField()
+    def __str__ (self):
+        return self.profile.__str__
 
-
-
-
-# class tutorUser (models.Model):
-#     # Will have to inherit from models.User
-
-#     # Below is a dummyVariable DELETE ME WHEN IMPLEMENTING
-
-#     dummy = models.CharField()
+# NOTE :: the student model is at the bottom as it includes some classes that are declarated later on
 
 
 
 
+class Course (models.Model):
+    name = models.CharField(max_length = 128)
+    level = models.PositiveSmallIntegerField ()
 
-# class course (models.Model):
-#     name = models.CharField()
-#     level = models.IntegerField()
-
-
-
-# class post (models.Model):
-#     content = models.CharField()
-#     timePosted = models.TimeField()
+    def __str__ (self):
+        return self.name
 
 
+class Lab (models.Model):
+    course = models.ForeignKey (Course)
+
+    tutors = models.ManyToManyField (Tutor)
+
+    labNumber = models.PositiveSmallIntegerField ()
+
+    def __str__ (self):
+        # "course::labNum"
+        return self.course.__str__ + "::" + self.labNumber
 
 
-# class lab (models.Model):
-#     labNum = models.IntegerField()
+
+class Post (models.Model):
+    # Using User Profile as it makes it easier 
+    # (though might need to change if we want to discern between
+    #  tutors and students in posts)
+    author = models.ForeignKey (UserProfile)
+    postedIn = models.ForeignKey (Lab)
+
+    content = models.TextField ()
+    timePosted = models.DateTimeField ()
+
+    def __str__ (self):
+        # "author::lab::time"
+        return self.author.__str__ + "::" + self.postedIn.__str__ + "::" + self.timePosted
+
+
+
+
+
+class Student(models.Model):
+    # Inherits from the userprofile
+    profile = models.OneToOneField (UserProfile)
+
+    # The symmetrical set to false as we do not want adding friends to add both ways
+    friends = models.ManyToManyField ("self", symmetrical=False)
+
+    labs = models.ManyToManyField (Lab)
+    courses = models.ManyToManyField (Course)
+
+    def __str__ (self):
+        return self.profile.__str__
+
