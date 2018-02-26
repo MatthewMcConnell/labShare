f509b72dbae9d26a4a49b5247e659a362635df82
Matthew McConnell <2235757M@student.gla.ac.uk>
2018-02-26 14:50:38 +0000

    Added a really basic base.html and added which models we need as comments

--- appLabShare/models.py
+++ appLabShare/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+# class studentUser (models.Model):
+#     # Will have to inherit from models.User
+
+#     # DummyVariable DELETE ME WHEN IMPLEMENTING
+
+#     dummy = models.CharField()
+
+
+
+
+# class tutorUser (models.Model):
+#     # Will have to inherit from models.User
+
+#     # Below is a dummyVariable DELETE ME WHEN IMPLEMENTING
+
+#     dummy = models.CharField()
+
+
+
+
+
+# class course (models.Model):
+#     name = models.CharField()
+#     level = models.IntegerField()
+
+
+
+# class post (models.Model):
+#     content = models.CharField()
+#     timePosted = models.TimeField()
+
+
+
+
+# class lab (models.Model):
+#     labNum = models.IntegerField()
