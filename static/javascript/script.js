6f08b8d2bdac75e7a1836a2820f228337e46adce
Eoin-McMahon <2253290m@student.gla.ac.uk>
2018-03-08 02:11:24 +0000

    EM: C Changed profile page and already logged in section

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,7 +1,7 @@
 
 var btn = document.querySelector('.mouse-cursor-gradient-tracking')
 
-btn.onmousemove = function(e) 
+btn.onmousemove = function(e)
 {
   var x = e.pageX - btn.offsetLeft - btn.offsetParent.offsetLeft
   var y = e.pageY - btn.offsetTop - btn.offsetParent.offsetTop
