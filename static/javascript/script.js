6a16945f9ca81fe4b00ed6e631c52ceb8034ea27
DominicHW <2253422h@student.gla.ac.uk>
2018-03-21 15:22:57 +0000

    DHW: Removed redundancy on embedded JS in HTML pages

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -9,20 +9,6 @@ btn.onmousemove = function(e)
   btn.style.setProperty('--y', y + 'px')
 }
 
-function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('#blah')
-                        .attr('src', e.target.result);
-                };
-
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
-
-
 function previewFile() {
     var preview = document.getElementById("blah");
     var file    = document.querySelector('input[type=file]').files[0];
@@ -36,3 +22,18 @@ function previewFile() {
         reader.readAsDataURL(file);
     }
   }
+
+function colour_randomiser(css_string) {
+  var colors = ['#90d1df', '#f4d30f', '#fd9900', '#805DA1', '#25876c'];
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  $(css_string).css('background-color', random_color);
+}
+
+function navbar_button() {
+  $(document).ready(function(){
+    $(".purpleButton").click(function(){
+      $("nav").slideToggle(500);
+      $("icons").slideToggle(500);
+    })
+  })
+}
